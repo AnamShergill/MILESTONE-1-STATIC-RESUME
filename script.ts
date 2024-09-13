1b532083b79275a33181cc76c5b5a5d24aa7bf42
Anam Shergill <pinkyshergill1986@gmail.com>
2024-09-13 08:18:13 +0500

    first commit

---
+++ script.ts
@@ -0,0 +1,12 @@
+// adding functionality to toggle button
+
+const button = document.getElementById('toggle-skills') as HTMLButtonElement;
+const skills = document.getElementById('skills') as HTMLElement;
+
+button.addEventListener('click', ()=>{
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block'
+    } else {
+        skills.style.display = 'none'
+    }
+});
